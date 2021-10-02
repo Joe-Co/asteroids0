d4f4f06960bc74697dffaa4aaf2e17ea7485670b
Joe-Co <54649633+Joe-Co@users.noreply.github.com>
2021-10-01 22:54:20 -0700

    Initial commit

---
+++ cohen/asteroids0/GamePlay.java
@@ -0,0 +1,6 @@
+package cohen.asteroids0;
+
+import javafx.scene.canvas.Canvas;
+
+public class GamePlay extends Canvas {
+}
---
+++ cohen/asteroids0/HelloController.java
@@ -0,0 +1,14 @@
+package cohen.asteroids0;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
